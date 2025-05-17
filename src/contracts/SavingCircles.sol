baefb0691fe0b24d2e6700a41d697b4117fc1b82
valeriooconte <valeriodomenicoconte@gmail.com>
2025-05-17 11:43:33 +0200

    refactor: removed function scoped variable in decomission() (Issue #15)

--- src/contracts/SavingCircles.sol
+++ src/contracts/SavingCircles.sol
@@ -129,9 +129,8 @@ contract SavingCircles is ISavingCircles, ReentrancyGuard, OwnableUpgradeable {
     // Return deposits to members
     for (uint256 i = 0; i < _circle.members.length; i++) {
       address _member = _circle.members[i];
-      uint256 _balance = balances[_id][_member];
 
-      if (_balance > 0) {
+      if (balances[_id][_member] > 0) {  
         balances[_id][_member] = 0;
         bool success = IERC20(_circle.token).transfer(_member, _balance);
         if (!success) revert TransferFailed();
